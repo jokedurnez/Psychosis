be7cfecb533e3d20ba3d650516121dd9e5563be9
jokedurnez <joke.durnez@gmail.com>
2018-03-12 10:54:46 -0700

    remove double folders

--- 05_connectome_group/00_collect_connectomes/combine_all.sh
+++
@@ -1,1 +0,0 @@
-singularity exec -B $OAK:$OAK -B $SCRATCH:$SCRATCH $CLEANSINGULARITY python $CODEDIR/05_connectome_group/00_collect_connectomes/combine_all.py
--- 05_connectome_group/02_compare_connectomes/collect.sh
+++
@@ -1,1 +0,0 @@
-singularity exec -B $OAK:$OAK -B $SCRATCH:$SCRATCH $CLEANSINGULARITY python $CODEDIR/05_connectome_group/02_compare_connectomes/collect.py
